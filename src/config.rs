889695b476d8f380a17b97679cc192f1acf1c4b0
shenzye <shenzye@hotmail.com>
2025-02-18 10:33:53 +0800

    add skip_serializing_if_none for IndexeddbConfig change package name

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,10 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Debug, Serialize, Deserialize, PartialEq, Hash, Clone)]
 #[serde(default)]
 pub struct IndexeddbConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub db_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub object_store_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub root: Option<String>,
 }
